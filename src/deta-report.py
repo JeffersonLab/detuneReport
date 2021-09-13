58514a543ab3d2427e52594c4ef2f392eec333c3
Adam Carpenter <adamc@jlab.org>
2021-09-13 10:28:36 -0400

    Fixed numpy warnings, html formatting

--- src/deta-report.py
+++ src/deta-report.py
@@ -62,6 +62,19 @@ class ResultSet:
         self.imgs[name] = cavity_results.img_bufs
         self.errors[name] = cavity_results.error_messages
 
+    def get_tdoff_stats(self, epics_name):
+        """ Calculate some basic statistics about the sample estimates."""
+        n_success = np.count_nonzero(~np.isnan(self.tdoff_errors[epics_name]))
+        if n_success > 0:
+            avg = round(np.nanmean(self.tdoff_errors[epics_name]), 3)
+            std = round(np.nanstd(self.tdoff_errors[epics_name]), 3)
+        else:
+            avg = math.nan
+            std = math.nan
+
+        return avg, std, n_success
+
+
     def to_table_string(self):
         """Return a table formatted human readable string."""
         t_fmt = "{:<7} {:>10} {:>10} {:>13} {:>13} {:>10}\n"
@@ -69,9 +82,7 @@ class ResultSet:
         out = t_fmt.format("Cavity", "TDOFF_New", "TDOFF_Cur", "TDOFF_Err_Avg",
                            "TDOFF_Err_Std", "N_Good")
         for name in sorted(self.epics_names):
-            avg = round(np.nanmean(self.tdoff_errors[name]), 3)
-            std = round(np.nanstd(self.tdoff_errors[name]), 3)
-            n_suc = np.count_nonzero(~np.isnan(self.tdoff_errors[name]))
+            avg, std, n_suc = self.get_tdoff_stats(name)
 
             tdoff = round(self.tdoffs[name], 3)
             tdoff_new = round(self.tdoffs[name] + avg, 3)
@@ -96,25 +107,25 @@ class ResultSet:
         """Return a html table of the data."""
 
         out = "<table>"
-        h_fmt = "<tr><th>{:<7}</th><th>{:>10}</th><th>{:>10}</th><th>{:>13}</th>"
-        h_fmt += "<th>{:>13}</th><th>{:>10}</th></tr>\n"
-        r_fmt = "<tr><td>{:<7}</td><td>{:>10}</td><td>{:>10}</td><td>{:>13}</td>"
-        r_fmt += "<td>{:>13}</td><td>{:>10}</td></tr>\n"
+        h_fmt = "<tr><th>{:<7}</th><th>{:>10}</th><th>{:>10}</th>"
+        h_fmt += "<th>{:>13}</th><th>{:>13}</th><th>{:>10}</th></tr>\n"
+        r_fmt = "<tr><td>{:<7}</td><td>{:>10}</td><td>{:>10}</td>"
+        r_fmt += "<td>{:>13}</td><td>{:>13}</td><td>{:>10}</td></tr>\n"
 
         # Set up the first summary table
+        out += "<thead>"
         out += h_fmt.format("Cavity", "TDOFF_New", "TDOFF_Cur", "TDOFF_Err_Avg",
                            "TDOFF_Err_Std", "N_Good")
+        out += "</thead><tbody>"
         for name in sorted(self.epics_names):
-            avg = round(np.nanmean(self.tdoff_errors[name]), 3)
-            std = round(np.nanstd(self.tdoff_errors[name]), 3)
-            n_suc = np.count_nonzero(~np.isnan(self.tdoff_errors[name]))
+            avg, std, n_suc = self.get_tdoff_stats(name)
 
             tdoff = round(self.tdoffs[name], 3)
             tdoff_new = round(self.tdoffs[name] + avg, 3)
             out += r_fmt.format(name, tdoff_new, tdoff, avg, std, n_suc)
 
         # Setup the second table for error messages
-        e_out = "</table><br><table>"
+        e_out = "</tbody></table><br><table>"
         h_fmt = "<tr><th>{:<7}</th><th>{:<5}</th><th>{}</th></tr>"
         r_fmt = "<tr><td>{:<7}</td><th>{:<5}</th><td>{}</td></tr>"
         e_out += "\n" + h_fmt.format("Cavity", "Run#", "Error_Message")
@@ -199,10 +210,10 @@ th {
         for name in sorted(rs.epics_names):
             self.message += f"<h2>{name}</h2>"
             cav_imgs = rs.imgs[name]
-            for img_buf in cav_imgs:
+            for run_num, img_buf in enumerate(cav_imgs):
                 if img_buf is None:
-                    self.message += f"<p>{name} experienced an error.</p>"
-                    self.message += f"<p>{rs.errors[name]}</p>\n"
+                    self.message += f"<p>{name} run {run_num+1} experienced an error.</p>"
+                    self.message += f"<p>{rs.errors[name][run_num]}</p>\n"
                 else:
                     string = base64.b64encode(img_buf.read()).decode()
                     uri = 'data:image/png;base64,' + string
